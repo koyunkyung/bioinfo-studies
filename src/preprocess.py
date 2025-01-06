c7242e73cba22ef719199a57bd2298b008ff50bc
Yunkyung Ko <koyunkyung@naver.com>
2025-01-06 13:55:01 +0900

    organize file structure

--- src/preprocess.py
+++ src/preprocess.py
@@ -4,9 +4,16 @@ import requests
 
 # Data preprocessing
 def preprocess(input_file, output_file):
+    # Load main dataset
     data = pd.read_excel(input_file, sheet_name=0)
     data.columns = data.columns.str.lower()
 
+    # Load cell_line_annotations.txt and create mapping
+    annotations = pd.read_csv("data/raw/cell_line_annotations.txt", sep="\t")
+    annotations = annotations[['Name', 'Disease']]
+    annotations.columns = ['cell_line_name', 'disease']
+
+    ## Filtering ##
     # Select relevant variables to use for the analysis
     columns_to_extract = ['cell_line_name', 'drug_name', 'putative_target', 'ln_ic50', 'auc', 'z_score']
     selected_data = data[columns_to_extract]
@@ -16,12 +23,22 @@ def preprocess(input_file, output_file):
         (data['z_score'] <= -2) | (data['z_score'] >= 2) &
         (data['auc'] <= 0.35) | (data['auc'] >= 0.85)
     ]
-    # Drop out NA values
+    # Drop out unnecessary columns and NA values
+    filtered_data = filtered_data.drop(columns=['auc', 'z_score'])
     filtered_data = filtered_data.dropna()
 
-    # Filter based on 'cell_line_name'
+    # Merge with annotations to add disease information
+    merged_data = pd.merge(filtered_data, annotations, on='cell_line_name', how='left')
 
+    # Group by 'disease' and aggregate
+    grouped_data = merged_data.groupby('disease').agg({
+        'cell_line_name': lambda x: ', '.join(x),  # Combine cell line names
+        'drug_name': lambda x: x.mode().iloc[0] if not x.mode().empty else 'Unknown',  # Get most frequent drug name
+        'putative_target': lambda x: x.mode().iloc[0] if not x.mode().empty else 'Unknown',  # Get most frequent target
+        'ln_ic50': 'mean'  # Calculate mean for ln_ic50
+    }).reset_index()
 
+    ## Encoding and Embedding ##
     # Encode categorical variables
     cell_line_encoder = LabelEncoder()
     drug_encoder = LabelEncoder()
