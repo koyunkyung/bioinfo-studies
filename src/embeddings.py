21490accca566cf2b00a37cd6ded58c78a0301a2
Yunkyung Ko <koyunkyung@naver.com>
2025-01-17 11:50:45 +0900

    update embedding results

--- src/embeddings.py
+++ src/embeddings.py
@@ -6,7 +6,7 @@ from rdkit.Chem import AllChem
 from rdkit.Chem.rdFingerprintGenerator import GetMorganGenerator
 from rdkit.Chem import MolFromSmiles, rdmolops
 from torch.nn import Module, Linear, ReLU
-from torch_geometric.nn import GATv2Conv, GCNConv, global_mean_pool
+from torch_geometric.nn import GATv2Conv, global_mean_pool
 from torch_geometric.data import Data, DataLoader
 from transformers import AutoTokenizer, AutoModel, BertTokenizer
 from performer_pytorch import PerformerLM
@@ -18,21 +18,21 @@ import safe
 class CellLineEmbedding:
     def __init__(self, scbert_model_path, num_bins=7, dim=200, depth=6, seq_len=16906, heads=10):
         
-        # scBERT 관련 초기화
-        self.seq_len = seq_len
-        self.num_bins = num_bins
-        self.scbert_model = PerformerLM(
-            num_tokens=num_bins + 2, 
-            dim=dim,
-            depth=depth,
-            max_seq_len=seq_len + 1,
-            heads=heads,
-            local_attn_heads=0,
-            g2v_position_emb=True,
-        )
-        ckpt = torch.load(scbert_model_path)
-        self.scbert_model.load_state_dict(ckpt["model_state_dict"])
-        self.scbert_model.eval()
+        # scBERT의 pre-trained model 경로 접근 권한 요청해야 함
+        # # scBERT 관련 초기화
+        # self.seq_len = seq_len
+        # self.num_bins = num_bins
+        # self.scbert_model = PerformerLM(
+        #     num_tokens=num_bins + 2, 
+        #     dim=dim,
+        #     depth=depth,
+        #     max_seq_len=seq_len + 1,
+        #     heads=heads,
+        #     local_attn_heads=0,
+        # )
+        # ckpt = torch.load(scbert_model_path)
+        # self.scbert_model.load_state_dict(ckpt["model_state_dict"])
+        # self.scbert_model.eval()
 
 
         # bioBERT 관련 초기화
@@ -241,9 +241,9 @@ if __name__ == "__main__":
     fingerprint = drug_embedding.Fingerprint()
     fromsmiles = drug_embedding.fromSMILES()
 
-    # scBERT embedding
-    scbert_embeddings = cell_embedding.scBERT(combined_cell_line)
-    print(f"scBERT Embeddings Shape: {scbert_embeddings.shape}")
+    # # scBERT embedding
+    # scbert_embeddings = cell_embedding.scBERT(combined_cell_line)
+    # print(f"scBERT Embeddings Shape: {scbert_embeddings.shape}")
 
 
     # bioBERT embedding
